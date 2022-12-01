22b1557b3678d1f07dadafdc77b98f85557efd16
Biser Karadzhov <85568905+Digital-Human-BK@users.noreply.github.com>
2022-12-01 11:34:30 +0200

    Responsive Nav and Home page

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 
 import hero from '../../../assets/hero.jpg';
+import heroPortrait from '../../../assets/herop.jpg';
 import useWalletCtx from '../../../hooks/useWalletCtx';
 
 import styles from './Home.module.css';
@@ -11,31 +12,35 @@ const Home = () => {
   const { isConnected, toggleWallet } = useWalletCtx();
 
   return (
-    <div className={styles.container}>
-      <h1 className={styles.welcome}>Welcome Beer Lover</h1>
-      <div className={styles.require_connect}>
-        {!isConnected && (
-          <>
-            <h2>Please connect your wallet to see the catalog with offers</h2>
-            <Button callback={toggleWallet} classes={'contained'}>
-              Connect Now!
-            </Button>
-          </>
-        )}
-        {isConnected && (
-          <>
-            <h2>Browse our awesome collection of beers</h2>
-            <Button callback={()=> navigate('/catalog')} classes={'contained'}>
-              Visit Catalog
-            </Button>
-          </>
-        )}
+    <>
+      <div className={styles.container}>
+        <h1 className={styles.welcome}>Welcome Beer Lover</h1>
+        <div className={styles.require_connect}>
+          {!isConnected && (
+            <>
+              <h2>Please connect your wallet to see the catalog with offers</h2>
+              <Button callback={toggleWallet} classes={'contained'}>
+                Connect Now!
+              </Button>
+            </>
+          )}
+          {isConnected && (
+            <>
+              <h2>Browse our awesome collection of beers</h2>
+              <Button
+                callback={() => navigate('/catalog')}
+                classes={'contained'}
+              >
+                Visit Catalog
+              </Button>
+            </>
+          )}
+        </div>
       </div>
-      <img src={hero} className={styles.hero} alt='beers' />
       <p className={styles.disclaimer}>
         Order wisely and with a measure. Cheers!
       </p>
-    </div>
+    </>
   );
 };
 
